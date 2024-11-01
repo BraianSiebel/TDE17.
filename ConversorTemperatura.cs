33ce8ae6e4fb474c509e18d46316ea546ea0a695
BraianSiebel <braiansiebelsiebel@gmail.com>
2024-10-31 23:15:08 -0300

    TDE17

---
+++ ConversorTemperatura.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public static class ConversorTemperatura
+{
+    public static double CelsiusParaFahrenheit(double celsius)
+    {
+        return (celsius*9/5)+32;
+    }
+    public static double FahrenheitParaCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(16));
+System.Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(16));
+
+Configuracoes.NomeAplicacao = "teste";
+Configuracoes.Versao = 1;
+
+System.Console.WriteLine($"nome aplicacao {Configuracoes.NomeAplicacao} e versao {Configuracoes.Versao}");
